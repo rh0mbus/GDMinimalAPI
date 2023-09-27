8c2e9124951f238f3ed8a78b308ab1ecaa4c8194
rh0mbus <wlegault_@hotmail.com>
2023-09-27 00:17:37 -0400

    added efcore and migrations

---
+++ PlayerStatsServer/DataContext.cs
@@ -0,0 +1,20 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace PlayerStatsServer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=playerstatsdb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<Player> Players => Set<Player>();
+    }
+}
---
+++ PlayerStatsServer/Migrations/20230927035301_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayerStatsServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Score = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
